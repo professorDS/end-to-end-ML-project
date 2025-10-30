4112091140f4c819db59c048d95efdd9eba04ae9
professorDS <professords06@gmail.com>
2025-10-30 18:48:43 +0530

    hyperparameter tuning

--- src/components/model_training.py
+++ src/components/model_training.py
@@ -47,10 +47,62 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
+            param_grids = {
+                            "Random Forest": {
+                            "n_estimators": [50, 100, 200],
+                            "max_depth": [None, 10, 20, 30],
+                            "min_samples_split": [2, 5, 10],
+                            "min_samples_leaf": [1, 2, 4]
+                            },
+
+                            "Decision Tree": {
+                            "criterion": ["squared_error", "friedman_mse", "absolute_error"],
+                            "splitter": ["best", "random"],
+                            "max_depth": [None, 5, 10, 20],
+                            "min_samples_split": [2, 5, 10],
+                            "min_samples_leaf": [1, 2, 4]
+                            },
+
+                            "Gradient Boosting": {
+                            "n_estimators": [100, 200],
+                            "learning_rate": [0.01, 0.05, 0.1],
+                            "subsample": [0.8, 1.0],
+                            "max_depth": [3, 5, 7]
+                            },
+
+                            "Linear Regression": {
+                            # Linear Regression has almost no hyperparameters to tune,
+                            # but we can check for normalization
+                            "fit_intercept": [True, False],
+                            "positive": [False, True]
+                            },
+
+                            "XGBRegressor": {
+                            "n_estimators": [100, 200],
+                            "learning_rate": [0.01, 0.05, 0.1],
+                            "max_depth": [3, 5, 7],
+                            "subsample": [0.8, 1.0],
+                            "colsample_bytree": [0.8, 1.0]
+                            },
+
+                            "CatBoosting Regressor": {
+                            "depth": [4, 6, 8],
+                            "learning_rate": [0.01, 0.05, 0.1],
+                            "iterations": [100, 200]
+                            },
+
+                            "AdaBoost Regressor": {
+                            "n_estimators": [50, 100, 200],
+                            "learning_rate": [0.01, 0.05, 0.1, 1.0],
+                            "loss": ["linear", "square", "exponential"]
+                            }
+                        }
+
+
             model_report={}
 
             model_report=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models)
+                                             models=models,param=param_grids)
             
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
