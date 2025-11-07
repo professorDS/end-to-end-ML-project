dbf242145c743f4e05cff1f0c8b7a090d0b2682a
professorDS <professords06@gmail.com>
2025-11-07 22:02:36 +0530

    prediction

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import dill
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+import pickle
 
 from src.exception import CustomException
 
@@ -52,3 +53,16 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     except Exception as e:
         raise CustomException(e, sys)
     
+
+
+
+
+def load_object(file_path):
+    try:
+        print(f"Loading object from: {file_path}")
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        print("Error while loading object:", e)
+        raise CustomException(e, sys)
+
